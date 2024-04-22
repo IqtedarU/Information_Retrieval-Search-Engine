91d30b08936fed15a5ec31b00fc6b4a763baa5a0
IqtedarU <113469923+IqtedarU@users.noreply.github.com>
2024-04-22 14:11:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify, render_template, send_file
+import time
+
+from flask import Flask, request, jsonify, render_template, send_file, redirect
 import os
 from search import search_query, load_index
 
@@ -11,13 +13,17 @@ def index():
 
 @app.route('/view_document/<string:title>', methods=['GET'])
 def view_document(title):
-    # Retrieve the file path of the document based on the doc_id
-    file_name = f"{title}.html"
+    # Retrieve the file path of the text file containing the URL
+    text_file_name = f"{title}.txt"
     directory = 'C:/website_content'
-    file_path = os.path.join(directory, file_name)
+    text_file_path = os.path.join(directory, text_file_name)
 
-    # Send the file to the user for viewing
-    return send_file(file_path, mimetype='text/html')
+    # Read the URL from the text file
+    with open(text_file_path, 'r', encoding='utf-8') as f:
+        url = f.readline().split(": ")[1].strip()
+
+    # Redirect the user to the URL
+    return redirect(url)
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -28,14 +34,19 @@ def search():
     index_file = "C:/index/inverted_index.pkl"  # Update with your index file path
     inverted_index = load_index(index_file)
 
+    start_time = time.time()
+
     # Perform search query processing
     search_results = search_query(query, inverted_index)
 
+    end_time = time.time()
+
+    search_time = end_time - start_time
     # Slice search_results to only include the first 15 results
     search_results = search_results[:15]
 
     # Render the search template with the search results
-    return render_template('search.html', query=query, search_results=search_results)
+    return render_template('search.html', query=query, search_results=search_results, search_time=search_time)
 
 
 if __name__ == '__main__':
