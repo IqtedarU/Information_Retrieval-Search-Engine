b695f5d820edf3b82ef8c95edd9297f62c0751ec
IqtedarU <113469923+IqtedarU@users.noreply.github.com>
2024-04-22 14:09:58 -0500

    Update web_spider.py

--- web_spider.py
+++ web_spider.py
@@ -6,20 +6,25 @@ class WebSpider(scrapy.Spider):
     allowed_domains = ['en.wikipedia.org']  # Limit crawling to Wikipedia
     start_urls = ['https://en.wikipedia.org/wiki/Main_Page']  # Starting URL
     max_pages = 10000  # Maximum number of pages to crawl
+    max_depth = 6
 
     def parse(self, response):
         # Extract content from the current page
         content = {
             'url': response.url,
             'title': response.css('title::text').get(),
-            'body': response.css('p::text').getall()+ response.css('h1::text').getall() + response.css('h2::text').getall() + response.css('h3::text').getall(),
+            'body': response.css('p::text').getall()+ response.css('h1::text').getall() + response.css('h2::text').getall() + response.css('h3::text').getall() + response.css('h4::text').getall() +
+            response.css('h5::text').getall() +
+            response.css('h6::text').getall() +
+            response.css('li::text').getall() +  # List items
+            response.css('.mw-parser-output > *::text').getall(),
         }
 
         # Save content to file
         self.save_content(content)
 
         # Extract links from the current page and follow them if within max depth and max pages limit
-        if response.meta.get('depth', 1) < 6 and self.max_pages > 0:
+        if response.meta.get('depth', 1) < self.max_depth and self.max_pages > 0:
             for link in response.css('a::attr(href)').getall():
                 if self.max_pages <= 0:
                     break
@@ -32,9 +37,15 @@ class WebSpider(scrapy.Spider):
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        # Construct file path using the URL
-        file_name = f"{content['title'].replace(' ', '_')}.html"  # Include doc_id in the file name
+
+        file_name = f"{content['title'].replace(' ', '_')}.html"
+        text_file_name = f"{content['title'].replace(' ', '_')}.txt"
         file_path = os.path.join(directory, file_name)
+        text_file_path = os.path.join(directory, text_file_name)
+
+        # Write URL to a separate text file
+        with open(text_file_path, 'w', encoding='utf-8') as f:
+            f.write(f"URL: {content['url']}\n")
 
         # Write HTML content to file
         with open(file_path, 'w', encoding='utf-8') as f:
